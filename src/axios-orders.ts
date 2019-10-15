5d7b5d37234d70ae80d993396dc9e610e7853935
Noah <osuolale49@gmail.com>
2019-10-15 23:24:11 +0100

    updated

--- src/axios-orders.ts
+++ src/axios-orders.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://noah-ninostyle-typescript-api.herokuapp.com'
-  // baseURL: 'http://localhost:4000'
 });
 
 export default instance;
