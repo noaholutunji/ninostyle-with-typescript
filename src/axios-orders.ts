20639713f52458f1aeb0f649332179db02bafb15
Noah Osuolale <osuolale49@gmail.com>
2022-07-21 06:27:58 +0100

    Modified axios order file

--- src/axios-orders.ts
+++ src/axios-orders.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://noah-ninostyle-typescript-api.herokuapp.com'
+  baseURL: 'https://noah-ninostyle-typescript-api.herokuapp.com'
 });
 
 export default instance;
